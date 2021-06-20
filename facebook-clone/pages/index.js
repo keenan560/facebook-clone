a75be2450df335c86eccbfd032379f16ae5265db
keenan560 <keenan560@gmail.com>
2021-06-20 13:41:21 -0500

    set up Next JS and tailwind.css now making Header compononent

---
+++ facebook-clone/pages/index.js
@@ -0,0 +1,19 @@
+import Head from "next/head";
+import Header from "../components/Header";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Facebook</title>
+      </Head>
+      {/* Header */}
+      <Header />
+      <main>
+        {/* Sidebar */}
+        {/* Feed */}
+        {/* Widgets */}
+      </main>
+    </div>
+  );
+}
